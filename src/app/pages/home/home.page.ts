0c87d6783c237b4f95a586095b6c7562ff737b80
David ML <darkslayer_zerox8@hotmail.com>
2022-10-06 14:30:39 -0500

    37. ion-list: Listas en ionic

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
 
+interface Componente {
+  icon: string;
+  name: string;
+  redirectTo: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
+
+  componentes: Componente[] = [
+    {
+      icon: 'cube-sharp',
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet',
+    },
+    {
+      icon: 'disc-sharp',
+      name: 'Alert',
+      redirectTo: '/alert',
+    }
+  ];
 
   constructor() {}
 

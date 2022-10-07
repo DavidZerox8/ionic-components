50a1e36b20a31c258fb3a876dcdd53f25ccffbe3
David ML <darkslayer_zerox8@hotmail.com>
2022-10-07 13:23:02 -0500

    44. ion-alert

--- src/app/pages/alert/alert.page.ts
+++ src/app/pages/alert/alert.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alert',
@@ -7,9 +8,101 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertPage implements OnInit {
 
-  constructor() { }
+  constructor(private alertController: AlertController) { }
 
   ngOnInit() {
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      backdropDismiss: false,
+      subHeader: 'Important message',
+      message: 'This is an alert!',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
+  async presentAlertButtons() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      backdropDismiss: false,
+      subHeader: 'Important message',
+      message: 'This is an alert!',
+      buttons: [
+        {
+          text: 'OK',
+          role: 'confirm',
+          handler: () => {
+            console.log('Click on OK');
+          },
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'red-destructive',
+          handler: () => {
+            console.log('Click on Cancel');
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+
+  }
+
+  async presentAlertPrompt() {
+    const alert = await this.alertController.create({
+      header: 'Please enter your info',
+      buttons: [
+        {
+          text: 'OK',
+          role: 'confirm',
+          handler: ( data: any ) => {
+            console.log(data);
+          },
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'red-destructive',
+          handler: () => {
+            console.log('Click on Cancel');
+          },
+        },
+      ],
+      backdropDismiss: false,
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Name',
+        },
+        {
+          name: 'nickname',
+          placeholder: 'Nickname (max 8 characters)',
+          attributes: {
+            maxlength: 8,
+          },
+        },
+        {
+          name: 'age',
+          type: 'number',
+          placeholder: 'Age',
+          min: 1,
+          max: 100,
+        },
+        {
+          name: 'description',
+          type: 'textarea',
+          placeholder: 'A little about yourself',
+        },
+      ],
+    });
+
+    await alert.present();
   }
 
 }
